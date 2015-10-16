e55d8d87dc88d1b0e0b2c6f865d51c0b4d3ea749
Jaime <xeven95@gmail.com>
2015-10-16 18:25:14 +0200

    Añadido 2 botones al inicio (Equipos y jugadores) con un listado simple

---
+++ Comunio/app/src/main/java/com/example/all_pc/myapplication/equipos.java
@@ -0,0 +1,21 @@
+package com.example.all_pc.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by All-PC on 16/10/2015.
+ */
+public class equipos extends Activity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lay_equipos);
+    }
+
+}
